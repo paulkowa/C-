96a8b96363097a0707eef29db2cf17350d90cbda
paulkowa <paulkowa@buffalo.edu>
2017-07-21 13:05:03 -0400

    Add files via upload

---
+++ Hr.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes;
+
+namespace ConsoleApplication1
+
+{
+    class Hr
+    {
+        static void Main(string[] args)
+        {
+
+            Associate a1 = new Associate(123, "Joel", 5.5);
+            Associate a2 = new Associate(234, "Vince", 5.5);
+            Associate a3 = new Associate(456, "Patrick", 5.5);
+            Manager m = new Manager(4892, "Ben", 10.5);
+            Department d = new Department("IT", m);
+
+            m.hire(a1);
+            m.hire(a2);
+            m.hire(a3);
+
+            Console.WriteLine(d.ToString());
+        }
+    }
+}
