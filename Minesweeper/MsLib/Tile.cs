9d0e89756a4662a7e8b11cd4cdb5cc9fe51896af
paulkowa <paulkowa@buffalo.edu>
2017-07-26 15:56:16 -0400

    Update Tile.cs

--- Minesweeper/MsLib/Tile.cs
+++ Minesweeper/MsLib/Tile.cs
@@ -8,14 +8,16 @@ namespace MsLib
 {
     public class Tile
     {
-        bool isActive { get; set; }
-        bool isMine { get; set; }
-        bool isFlagged { get; set; }
-        int nearbyMines { get; set; }
+        public bool isActive { get; set; }
+        public bool isMine { get; set; }
+        public bool isFlagged { get; set; }
+        public int nearbyMines { get; set; }
 
         public Tile()
         {
-
+            isActive = false;
+            isMine = false;
+            isFlagged = false;
         }
 
         public Tile(bool isMine)
@@ -29,48 +31,6 @@ namespace MsLib
         {
             if(isMine) { return 1; }
             return 0;
-        }
-
-        public void setMineCount(ref List<Tile> board)
-        {
-            int tileCount = board.Capacity;
-            //Easy
-            if (tileCount == 64) { }
-            //Medium
-            else if (tileCount == 256) { }
-
-            //Hard difficulty
-            else {
-                for (int i = 0; i < tileCount; i++)
-                {
-                    if (i == 0 || i == 23 || i == 552 || i == 575)
-                    {
-                        switch (i)
-                        {
-                            case 0:
-                                nearbyMines = board[i + 1].toInt() + board[i + 25].toInt() + board[i + 24].toInt();
-                                break;
-                            case 23:
-                                nearbyMines = board[i - 1].toInt() + board[i + 23].toInt() + board[i + 24].toInt();
-                                break;
-                            case 552:
-                                nearbyMines = board[i + 1].toInt() + board[i - 23].toInt() + board[i - 24].toInt();
-                                break;
-                            case 575:
-                                nearbyMines = board[i - 1].toInt() + board[i - 25].toInt() + board[i - 24].toInt();
-                                break;
-                        }
-                    }
-                    else
-                    {
-                        if (i % 24 == 23) { nearbyMines = board[i + 24].toInt() + board[i + 23].toInt() + board[i - 1].toInt() + board[i - 25].toInt() + board[i - 24].toInt(); }
-                        else if (i % 24 == 0) { nearbyMines = board[i + 1].toInt() + board[i + 25].toInt() + board[i + 24].toInt() + board[i - 24].toInt() + board[i - 23].toInt(); }
-                        else if (i - 24 < 0) { nearbyMines = board[i + 1].toInt() + board[i + 25].toInt() + board[i + 24].toInt() + board[i + 23].toInt() + board[i - 1].toInt(); }
-                        else if (i + 24 > 575) { nearbyMines = board[i + 1].toInt() + board[i - 1].toInt() + board[i - 25].toInt() + board[i - 24].toInt() + board[i - 23].toInt(); }
-                        else { nearbyMines = board[i + 1].toInt() + board[i + 25].toInt() + board[i + 24].toInt() + board[i +23].toInt() + board[i - 1].toInt() + board[i - 25].toInt() + board[i - 24].toInt() + board[i - 23].toInt(); }
-                    }
-                }
-            }
         }
     }
 }
