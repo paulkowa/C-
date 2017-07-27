f30d36b69e57263799512d49d18a7022ab6266c4
paulkowa <paulkowa@buffalo.edu>
2017-07-27 16:24:30 -0400

    Add files via upload

---
+++ Minesweeper/MsLib/Ms/Game.cs
@@ -0,0 +1,59 @@
+﻿using Ms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace MsLib
+{
+    class Game
+    {
+        public List<MineButton> buttons;
+        private MainWindow window;
+        private Board board;
+
+        public Game(MainWindow window)
+        {
+            this.window = window;
+            board = new Board();
+            buttons = new List<MineButton>();
+            newGame();
+        }
+
+        private void newGame()
+        {
+            fillBoard();
+        }
+
+        private void fillBoard()
+        {
+            window.UniGrid.Columns = 24;
+            window.UniGrid.Rows = 24;
+
+            int index = 0;
+            for (int row = 0; row < 24; row++)
+            {
+                for (int column = 0; column < 24; column++)
+                {
+                    MineButton mineButton = new MineButton(board.board[index], buttons, index);
+                    buttons.Add(mineButton);
+                    window.UniGrid.Children.Add(mineButton);
+                    index++;
+
+                    //Button mineButton = new Button();
+                    //Image mine = new Image();
+                    //mine.Source = new BitmapImage(new Uri(@"/Resources/mine.png", UriKind.RelativeOrAbsolute));
+                    //mine.VerticalAlignment = VerticalAlignment.Center;
+                    //mineButton.Content = mine;
+                    //buttons.Add(mineButton);
+                    //window.UniGrid.Children.Add(mineButton);
+                    //index++;
+                }
+            }
+        }
+    }
+}
---
+++ Minesweeper/MsLib/Ms/MineSweeper.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using MsLib;
+
+namespace Ms
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Game g;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            g = new Game(this);
+        }
+    }
+}
