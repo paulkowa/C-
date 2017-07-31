daa47576f624bdbeadbc65e90ff736864af7330f
paulkowa <paulkowa@buffalo.edu>
2017-07-31 16:15:57 -0400

    Add files via upload

--- Minesweeper/MsLib/Ms/Board.cs
+++ Minesweeper/MsLib/Ms/Board.cs
@@ -8,75 +8,16 @@ namespace MsLib
 {
     public class Board
     {
-        private int size;
-        public double mineTotal { get; private set; }
-        public List<Tile> board { get; private set; }
-        public Board(int size)
+        List<Tile> board;
+
+        public Board()
         {
-            // Initialize values
-            this.size = size;
-            board = new List<Tile>();
-            // Fill board, assign mines, and assign tile mine count
-            fillBoard(board, this.size);
-            generateMines(board, size);
-            mineCount(board, size);
-        }
-        // Fill the board array with the correct number of tiles
-        private void fillBoard(List<Tile> board, int size)
-        {
-            for (int i = 0; i < size; i++) { board.Add(new Tile(i)); }
+            board = new List<Tile>(576);
+            Console.WriteLine(board.Count);
         }
 
-        // Assign tiles as mines at random
-        private void generateMines(List<Tile> board, int size)
+        private void fillBoard(List<Tile> board)
         {
-            Random rand = new Random();
-            double mines = Math.Round(size * 0.2);
-            mineTotal = mines;
-            while (mines > 0)
-            {
-                int i = rand.Next(0, size - 1);
-                if (board[i].isMine == false)
-                {
-                    board[i].isMine = true;
-                    mines--;
-                }
-            }
-        }
-
-        // Assign tiles with their correct nearby mine values
-        private void mineCount(List<Tile> board, int size)
-        {
-            for (int i = 0; i < board.Count; i++)
-            {
-                if (i == 0 || i == 23 || i == 552 || i == 575)
-                {
-                    switch (i)
-                    {
-                        case 0:
-                            board[i].nearbyMines = board[i + 1].toInt() + board[i + 25].toInt() + board[i + 24].toInt();
-                            break;
-                        case 23:
-                            board[i].nearbyMines = board[i - 1].toInt() + board[i + 23].toInt() + board[i + 24].toInt();
-                            break;
-                        case 552:
-                            board[i].nearbyMines = board[i + 1].toInt() + board[i - 23].toInt() + board[i - 24].toInt();
-                            break;
-                        case 575:
-                            board[i].nearbyMines = board[i - 1].toInt() + board[i - 25].toInt() + board[i - 24].toInt();
-                            break;
-                    }
-                }
-                else
-                {
-                    if (i % 24 == 23) { board[i].nearbyMines = board[i + 24].toInt() + board[i + 23].toInt() + board[i - 1].toInt() + board[i - 25].toInt() + board[i - 24].toInt(); }
-                    else if (i % 24 == 0) { board[i].nearbyMines = board[i + 1].toInt() + board[i + 25].toInt() + board[i + 24].toInt() + board[i - 24].toInt() + board[i - 23].toInt(); }
-                    else if (i < 23) { board[i].nearbyMines = board[i + 1].toInt() + board[i + 25].toInt() + board[i + 24].toInt() + board[i + 23].toInt() + board[i - 1].toInt(); }
-                    else if (i > 552) { board[i].nearbyMines = board[i + 1].toInt() + board[i - 1].toInt() + board[i - 25].toInt() + board[i - 24].toInt() + board[i - 23].toInt(); }
-                    else { board[i].nearbyMines = board[i + 1].toInt() + board[i + 25].toInt() + board[i + 24].toInt() + board[i + 23].toInt() + board[i - 1].toInt() + board[i - 25].toInt() + board[i - 24].toInt() + board[i - 23].toInt(); }
-                }
-            }
         }
     }
 }
-
